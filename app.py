00aaaef8d40a3bd91cf4c57c9ee8e7af0579d89a
Anascalle <127751671+Anascalle@users.noreply.github.com>
2025-06-11 23:25:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-from backend.rawg_api import fetch_games, fetch_game_by_id, fetch_genres, fetch_platforms
+from rawg_api import fetch_games, fetch_game_by_id, fetch_genres, fetch_platforms
+import os
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:5173"])
+CORS(app)  # Permitir todas las conexiones para desarrollo
+
+@app.route('/')
+def home():
+    return "🎮 API de videojuegos activa en Render"
 
 @app.route('/games', methods=['GET'])
 def get_games():
     data = fetch_games()
-    # Retornas solo la lista de resultados para simplificar
     return jsonify(data['results'])
 
 @app.route('/games/<int:game_id>', methods=['GET'])
@@ -27,4 +31,6 @@ def get_platforms():
     return jsonify(data['results'])
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
+
